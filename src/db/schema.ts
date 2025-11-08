a01490b5c331e137d55ca947530373bccd51c2fa
Sagar <sagar.icubes@gmail.com>
2025-11-08 13:58:04 +0000

    changes done

---
+++ src/db/schema.ts
@@ -0,0 +1,7 @@
+import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 256 }).notNull(),
+  email: text('email').notNull(),
+});
